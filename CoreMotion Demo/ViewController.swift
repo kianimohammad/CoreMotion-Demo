9e201248ba0b12d2b2767eac916f6602402b9f1c
Mohammad Kiani <kianimohammad@outlook.com>
2020-01-21 16:08:47 -0500

    Initial Commit

---
+++ CoreMotion Demo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CoreMotion Demo
+//
+//  Created by Mohammad Kiani on 2020-01-21.
+//  Copyright © 2020 mohammadkiani. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
